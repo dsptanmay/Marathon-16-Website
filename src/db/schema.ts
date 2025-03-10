da0ea363e1d485f0d0f8e93a84fabd693e119b2e
yuvraj singh <13742shyuvraj@gmail.com>
2025-03-10 13:12:20 +0530

    Added a new feature

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,5 +1,16 @@
-import { pgTable, text } from "drizzle-orm/pg-core";
+import { pgTable, serial, text, boolean, timestamp } from "drizzle-orm/pg-core";
 
-export const users = pgTable("users", {
-  id: text("id").primaryKey(),
+
+export const master = pgTable("master", {
+  id: serial("id").primaryKey(), // Auto-incrementing ID
+  unique_code: text("unique_code").notNull(),
+  name: text("name").notNull(),
+  email: text("email").notNull(),
+  phone_no: text("phone_no").notNull(),
+  usn: text("usn").notNull(),
+  category: text("category").notNull(),
+  isCrossed: boolean("isCrossed").default(false),
+  crossTime: timestamp("crossTime", { withTimezone: true }).defaultNow(),
+  isSitian: boolean("isSitian").default(false),
+  qrcodedata: text("qrcodedata"),
 });
