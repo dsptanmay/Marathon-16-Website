d177ed8280a815dda3537f3b60ded2655ee73dc9
yuvraj singh <13742shyuvraj@gmail.com>
2025-03-12 20:08:48 +0530

    Added a new feature

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,11 +1,13 @@
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
 import userRouter from "./users";
+import registerRouter from "./signup";
 
 const app = new Hono().basePath("/api");
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const routes = app.route("/users", userRouter);
+app.route("/register", registerRouter);
 
 export const GET = handle(app);
 export const POST = handle(app);
---
+++ src/app/api/[[...route]]/signup.ts
@@ -0,0 +1,55 @@
+import { db } from "@/db";
+import { Hono } from "hono";
+import { master } from "@/db/schema";
+import { masterSchema } from "@/validations/masterSchema";
+import { zValidator } from "@hono/zod-validator";
+
+type UserInput = {
+  unique_code: string;
+  name: string;
+  email?: string;
+  phone_no: string;
+  usn?: string;
+  age: number;
+};
+
+const registerRouter = new Hono();
+
+const registerHandler = async (c: any) => {
+  try {
+    const body = await c.req.json() as UserInput;  
+    const { unique_code, name, email, phone_no, usn, age } = body;
+
+    if (!unique_code || !name || !phone_no) {
+      return c.json({ error: "Missing required fields" }, 400);
+    }
+
+    const userData: Partial<UserInput> = { unique_code, name, phone_no, age };
+
+    if (age) userData.email = email;
+    if (usn) userData.usn = usn;
+
+    await db.insert(master).values(userData);
+
+    return c.json({ message: "User registered successfully" });
+  } catch (error) {
+    return c.json({ error: "Something went wrong", details: error.message }, 500);
+  }
+};
+
+const validationMiddleware = zValidator(
+  "json",
+  masterSchema.omit({
+    category: true,
+    isCrossed: true,
+    crossTime: true,
+    isSitian: true,
+    qrcodedata: true,
+  })
+);
+
+registerRouter.post("/girls", validationMiddleware, registerHandler);
+registerRouter.post("/boys", validationMiddleware, registerHandler);
+registerRouter.post("/walkathon", validationMiddleware, registerHandler);
+
+export default registerRouter;
