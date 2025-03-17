d10f27e36438a3c2b036b7b3f7dfd2fe37951f28
yuvraj singh <13742shyuvraj@gmail.com>
2025-03-17 21:35:37 +0530

    /yuvraj//complete girls and walkathon mutation

--- src/hooks/use-register-user.ts
+++ src/hooks/use-register-user.ts
@@ -1,9 +1,16 @@
 import { api } from "@/lib/hono";
 import { useMutation } from "@tanstack/react-query";
 import { InferRequestType, InferResponseType } from "hono";
+import { Variable } from "lucide-react";
 
 type ResponseTypeBoys = InferResponseType<typeof api.register.boys.$post, 201>;
 type RequestTypeBoys = InferRequestType<typeof api.register.boys.$post>["json"];
+
+type ResponseTypeGirls = InferResponseType<typeof api.register.girls.$post, 201>;
+type RequestTypeGirls = InferRequestType<typeof api.register.girls.$post>["json"];
+
+type ResponseTypeWalkathon = InferResponseType<typeof api.register.walkathon.$post, 201>;
+type RequestTypeWalkathon = InferRequestType<typeof api.register.walkathon.$post>["json"];
 
 export const useRegisterBoys = () => {
   const mutation = useMutation<ResponseTypeBoys, Error, RequestTypeBoys>({
@@ -27,5 +34,38 @@ export const useRegisterBoys = () => {
 
 // TODO: write custom hooks for registering girls and walkathon members
 
-export const useRegisterGirls = () => {};
-export const useRegisterWalkathon = () => {};
+export const useRegisterGirls = () => {
+  const mutation = useMutation<ResponseTypeGirls, Error, RequestTypeGirls>({
+    mutationFn: async (data) => {
+      const response = await api.register.girls.$post({ json: data }); // POST call -> /api/register/girls/
+      if (!response.ok) throw new Error("Failed to register participant");
+      return await response.json();
+    },
+    onSuccess: async (data, variables) => {
+      console.log(`${variables.name} registered successfully`);
+    },
+    onError: async (error, data) => {
+      console.log(`Failed to register user ${data.name}`);
+      console.error(error);
+    },
+  });
+  return mutation;
+};
+
+export const useRegisterWalkathon = () => {
+  const mutation = useMutation<ResponseTypeWalkathon, Error, RequestTypeWalkathon>({
+    mutationFn: async (data) => {
+      const response = await api.register.walkathon.$post({ json: data }); // POST call -> /api/register/walkathon/
+      if (!response.ok) throw new Error("Failed to register participant");
+      return await response.json();
+    },
+    onSuccess: async (data, variables) => {
+      console.log(`${variables.name} registered successfully`);
+    },
+    onError: async (error, data) => {
+      console.log(`Failed to register user ${data.name}`);
+      console.error(error);
+    },
+  });
+  return mutation;
+};
