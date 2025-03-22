10908532e1c42fd774efd9e5b08f8e1437d12c6b
Tanmay Deshpande <dsp.tanmay@gmail.com>
2025-03-22 08:09:21 +0530

    userinfo fixes

--- src/app/api/[[...route]]/userinfo.ts
+++ src/app/api/[[...route]]/userinfo.ts
@@ -1,49 +1,48 @@
 import { db } from "@/db";
 import { Hono } from "hono";
 import { masterTable } from "@/db/schema";
-import { eq } from "drizzle-orm";
+import { and, eq } from "drizzle-orm";
 import { z } from "zod";
 import { zValidator } from "@hono/zod-validator";
-import { InferSelectModel } from "drizzle-orm";
 
-type MasterTable = InferSelectModel<typeof masterTable>;
-
+// type MasterTable = InferSelectModel<typeof masterTable>;
 
 const getUserInfoSchema = z.object({
-  unique_code: z.string().min(6, "unique_code is required"),
+  unique_code: z
+    .string()
+    .min(6, "unique_code is required")
+    .regex(/^[A-Z]{5}\d{1}$/),
 });
 
-const userRouter = new Hono()
-  .get(
-    "/userinfo",
-    zValidator("query", getUserInfoSchema),
-    async (c) => {
-      const { unique_code } = c.req.valid("query");
+const userRouter = new Hono().get(
+  "/userinfo",
+  zValidator("query", getUserInfoSchema),
+  async (c) => {
+    const { unique_code } = c.req.valid("query");
 
-      try {
-        const user = await db
-          .select()
-          .from(masterTable)
-          .where(
+    try {
+      const user = await db
+        .select()
+        .from(masterTable)
+        .where(
+          and(
             eq(masterTable.unique_code, unique_code), // Checking for matching unique_code
             eq(masterTable.isCrossed, true) // Ensuring isCrossed is true
           )
-          .execute();
+        )
+        .execute();
 
-        if (user.length === 0) {
-          return c.json({ error: "User not found or has not crossed yet" }, 404);
-        }
-
-        const userData: MasterTable = user[0];
-        return c.json(userData, 200);
-      } catch (error) {
-        console.error("Error fetching user info:", error);
-        return c.json(
-          { error: "Internal server error", details: error.message },
-          500
-        );
+      if (user.length === 0) {
+        return c.json({ error: "User not found or has not crossed yet" }, 404);
       }
+
+      const userData = user[0];
+      return c.json(userData, 200);
+    } catch (error) {
+      console.error("Error fetching user info:", error);
+      return c.json({ error: "Internal server error" }, 500);
     }
-  );
+  }
+);
 
 export default userRouter;
